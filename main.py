7818492c03915e7394e7f9d79be2f51aa730d672
GurpreetKaur <gurpreetkaur.qe@gmail.com>
2020-11-09 16:00:06 +0530

    first addition:added role model till number3

---
+++ main.py
@@ -0,0 +1,14 @@
+print("*******************************************************")
+print("Confused to choose your role model??\nDon't worry! now you are correct place! :)")
+
+i=int(input("\nEnter any number between 1-10:"))
+
+print("Great! Your role model is")
+if i==1:
+    print("Guru Nanak Dev Ji")
+elif i==2:
+    print("Mata Bhag Kaur Ji")
+elif i==3:
+    print("Baba Ajit Singh Ji")
+else:
+    print("Please enter number between 1-10")
